c98dac0fdc2472dcb132aac5b876ae06ac7574cc
Daniel Fontiveros Macias <dfontive@c1r1s4.42malaga.com>
2024-12-18 20:19:30 +0100

    aeiou

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dfontive <dfontive@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 14:50:50 by dfontive          #+#    #+#             */
-/*   Updated: 2024/12/18 19:04:25 by dfontive         ###   ########.fr       */
+/*   Updated: 2024/12/18 20:19:04 by dfontive         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,47 @@
 
 int	ft_printf(const char *format, ...)
 {
-	int	i;
+	int		i;
+	int		x;
+	va_list	arg;
 
 	i = 0;
+	x = 0;
+	va_start (arg, format);
 	while (format[i])
 	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
+			{
+				char c = va_arg (arg, char);
+				c_putchar(c);
+				i += 2;
+			}
+			if (format [i + 1] == 's')
+			{}
+			if (format [i + 1] == 'p')
+			{}
+			if (format [i + 1] == 'd')
+			{}
+			if (format [i + 1] == 'i')
+			{}
+			if (format [i + 1] == 'u')
+			{}
+			if (format [i + 1] == 'x')
+			{}
+			if (format [i + 1] == 'X')
+			{}
+			if (format [i + 1] == '%')
+			{}
+		}
 		write(1, &format[i], 1);
 		i++;
 	}
-	
+	return (i + x);
 }
 
 int	main(void)
 {
-	
+	ft_printf("Hola, me llamo %d y tengo %s años", 343, "Lola");
 }
--- printf.h
+++ printf.h
@@ -2,7 +2,10 @@
 # define PRINTF_H
 
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
+void	c_putchar(char c);
 
 #endif
